10b517f880729c2ad115eeea38daab6ff4a32009
Aya Emad <ayaelbaroudy0@gmail.com>
2025-09-08 20:46:58 +0300

    add feature search

--- lib/screens/home/tabs/time/time_tab.dart
+++ lib/screens/home/tabs/time/time_tab.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
-
-import '../../../../core/utils/app_asset.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class TimeTab extends StatelessWidget {
   const TimeTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return ClipPath(
+        //clipper: WaveClipperTwo(reverse: true,),
+        //clipper: WaveClipperTwo(),
+       //clipper:  WaveClipperOne(reverse: true),
+      //clipper:   WaveClipperOne(),
+    child: Container(
+    width: 100,
+    height: 50,
+    color: Colors.amber[300],
+    ),
     );
-  }
-}
+    }
+    }
+
